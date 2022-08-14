2dbd1359e59ab4c12783da4a2c88cd4dedc6a121
amiraliGH500 <amiralighasemi@yahoo.com>
2022-08-14 10:18:42 +0430

    second commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
             <h1 className='w-96 m-auto mt-28'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، </h1>
             <button className='bg-cyan-700  shadow-xl rounded-lg h-10 w-32 m-auto active:shadow-none hover:bg-cyan-400 hover:text-white'>لورم ایپسوم متن </button>
             </div>
-            <img
+            <Image
             layout=''
             className='object-cover m-5 w-96 rounded-xl'
             src='/pexels-gabii-fernandez-11545053.jpg' alt='yooo'/>
